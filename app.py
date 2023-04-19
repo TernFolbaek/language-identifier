f8bc41e26bc61db12165ce6ac18ed320efff8e82
Tern Folbaek <tfolbaek@gmail.com>
2023-04-19 19:58:31 +0200

    variable fixes

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ import webbrowser
 
 global Lrdetect_Model
 
-LrdetectFile = open('model.pckl', 'rb')
-Lrdetect_Model = pickle.load(LrdetectFile)
-LrdetectFile.close()
+pickle_file = open('model.pckl', 'rb')
+lr_model = pickle.load(pickle_file)
+pickle_file.close()
 st.title("Language detection Tool")
 input_test = st.text_input("provide your text input here", "Hello my name is Jay")
 
 button_clicked = st.button("Get Language Name")
 if button_clicked:
-    st.text(Lrdetect_Model.predict([input_test]))
+    st.text(lr_model.predict([input_test]))
 
